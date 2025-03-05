e3f98ef470cce65187862ca8d9efa1fd02366dc7
CristopherUCLM <cristopherazariel.santaiti1@alu.uclm.es>
2025-03-05 13:54:16 +0100

    marcos

--- FPII_Lab/src/HITO_II/Main.java
+++ FPII_Lab/src/HITO_II/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//Creo al entrenador
-		
+		//Marcos encargate de esto
 		//Creo las pokeballs con integridad a 10
 		Pokeball [] pokeballs = new Pokeball[6];
 		for(int i = 0; i<pokeballs.length;i++) {

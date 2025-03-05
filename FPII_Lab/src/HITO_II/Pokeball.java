f56756731191277daf63b171e1d564d718ea9ff7
CristopherUCLM <cristopherazariel.santaiti1@alu.uclm.es>
2025-03-05 13:53:32 +0100

    hito II package?

---
+++ FPII_Lab/src/HITO_II/Pokeball.java
@@ -0,0 +1,17 @@
+package HITO_II;
+
+public class Pokeball {
+	protected int energia;
+	
+	public Pokeball(int e) {
+		this.energia = e;
+	}
+	
+	public int getEnergia() {
+		return this.energia;
+	}
+	
+	public void setEnergia(int e) {
+		this.energia = e;
+	}
+}
